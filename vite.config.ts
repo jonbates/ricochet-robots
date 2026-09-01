128c5bd1c91bffde555e957d6adbc58e9c4d3d62
Jon Bates <jon.c.bates@gmail.com>
2026-09-01 08:13:02 -0600

    update for v1.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/rr-v1/', // forces relative paths (e.g., "assets/app.js" instead of "/assets/app.js")
+  base: '/rr-v1-1/', // forces relative paths (e.g., "assets/app.js" instead of "/assets/app.js")
   plugins: [tailwindcss()],
 });
