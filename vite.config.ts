3e444b637c84020294d99eccd31f942fb5dd12d8
Jon Bates <jon.c.bates@gmail.com>
2026-09-02 07:05:12 -0600

    hide chunks sizes less < 750k. update vulnerabilities

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 750,
+  },
   base: '/rr-v1-1/', // forces relative paths (e.g., "assets/app.js" instead of "/assets/app.js")
   plugins: [tailwindcss()],
 });
